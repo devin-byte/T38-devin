e656bd80b0a63a9c71f06f80e81ab0556dd2b925
Jonie Miralda <jonie.mirc@gmail.com>
2026-02-04 19:09:32 -0600

    Ejercios Post y PUT

--- MiPrimerProyecto/app.js
+++ MiPrimerProyecto/app.js
@@ -2,10 +2,43 @@ const express = require('express');
 const app = express();
 const PORT = 3000;
 
+app.use(express.json());
+
 const users = [{usuario:'Jonie Miralda', edad:'33'},{usuario:'Hugo Duron', edad:'35'},{usuario:'Oscar Ucles', edad:'40'}];
+
 app.get('/usuarios',(req, res)=>{
     
     res.json({status:200,message:'Success', data: users});
+});
+
+app.post('/usuarios',(req, res)=>{
+
+    const usuario = req.body;
+    users.push(usuario);
+    res.status(201).json({status:201,message:'Usuario se registro exitosamente.', data: usuario});
+
+});
+
+app.put('/usuarios/:nombre',(req, res)=>{
+    const usuario = req.body;
+    const nombre = req.params.nombre;
+
+    let isExists = false;
+    users.forEach(user => {
+        if(user.usuario === nombre){
+            isExists = true;
+            user.edad = usuario.edad;
+            user.nombre = usuario.nombre;
+        }
+    });
+
+    if(isExists){
+        res.status(200).json({status:200,message:'Usuario se actualizo exitosamente.', data: usuario});
+    }
+    else{
+        res.status(404).json({status:404,message:'Usuario no encontrado...'});
+    }
+    
 });
 
 app.listen(PORT, ()=>{
